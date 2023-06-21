33baefc83adc091450450ef7622d1d6220bab394
bazeeel <deakfefe77@gmail.com>
2023-06-21 05:08:12 +0200

    nix config

---
+++ configuration.nix
@@ -0,0 +1,322 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+
+{
+  
+  nixpkgs.config.permittedInsecurePackages = [
+                "openssl-1.1.1u"
+		"python-2.7.18.6"
+              ];
+
+  #hardware.opengl.driSupport32Bit = true;
+  #hardware.pulseaudio.support32Bit = true;
+  nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem (builtins.parseDrvName pkg.name).name [ "steam" "steam-original" "steam-run" ]);
+  nix.settings = {
+    substituters = ["https://nix-gaming.cachix.org"];
+    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+ 
+};
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Budapest";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "hu_HU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "hu_HU.UTF-8";
+    LC_IDENTIFICATION = "hu_HU.UTF-8";
+    LC_MEASUREMENT = "hu_HU.UTF-8";
+    LC_MONETARY = "hu_HU.UTF-8";
+    LC_NAME = "hu_HU.UTF-8";
+    LC_NUMERIC = "hu_HU.UTF-8";
+    LC_PAPER = "hu_HU.UTF-8";
+    LC_TELEPHONE = "hu_HU.UTF-8";
+    LC_TIME = "hu_HU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Pantheon Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  
+  # services.xserver.desktopManager.pantheon.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+                         
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "hu";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "hu101";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+   services.picom.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.xeoncpu = {
+    isNormalUser = true;
+    description = "xeoncpu";
+    extraGroups = [ "networkmanager" "wheel" "input" "disk" ];
+   # packages = with pkgs; [   
+    #  thunderbird
+    #];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "xeoncpu";
+
+ ## Gaming
+	programs.steam = {
+	  enable = true;
+	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	}; 
+ 
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+  };
+   
+  fonts.fonts = with pkgs; [
+    noto-fonts-emoji
+    dejavu_fonts
+    liberation_ttf
+    source-code-pro
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config = {
+    allowUnfree = true;
+    joypixels.acceptLicense = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+  hardware.nvidia.modesetting.enable = true;
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    # wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+      
+  
+ };
+
+  environment.variables.EDITOR = "nvim";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+     vim
+     feh
+     gnugrep
+     gnumake
+     polkit_gnome
+     xdg-desktop-portal-gtk
+     alacritty
+     mpv
+     ueberzug
+     exa
+     i3blocks
+     zsh
+     htop
+     vifm
+     git
+     discord
+     unzip
+     unrar
+     xclip
+     neovim
+     neofetch
+     maim
+     picom
+    urlscan
+    socat
+    ntfs3g
+    openssl
+    protonup-ng
+    microsoft-edge
+    heroic
+    sublime3
+    killall
+    mpd
+    ncmpcpp
+    feh
+    xwallpaper
+    sxiv
+    fzf
+    sxhkd
+    ffmpeg
+    lxappearance
+    pavucontrol
+    pcmanfm
+    bc
+    lm_sensors
+    bat
+    acpi
+    xcape
+    xdo
+    xdotool
+    dunst
+    wmctrl
+    samba
+    cifs-utils
+    wsdd
+    steam
+    steam-run
+    
+  ];
+
+ security.polkit.enable = true;
+ systemd = {
+  user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+  };
+   extraConfig = ''
+     DefaultTimeoutStopSec=10s
+   '';
+}; 
+
+	
+# services.samba = { enable = true; # make shares visible for windows 10 clients
+networking.firewall.allowedTCPPorts = [
+  445 139 # wsdd
+];
+networking.firewall.allowedUDPPorts = [
+  137 138 # wsdd
+];
+
+
+services.samba = { 
+  enable = true;
+  securityType = "user";
+  extraConfig = ''
+    workgroup = WORKGROUP
+    server string = smbnix
+    netbios name = smbnix
+    security = user 
+    #use sendfile = yes
+    #max protocol = smb2
+    # note: localhost is the ipv6 localhost ::1
+    hosts allow = 192.168.0. 127.0.0.1 localhost
+    hosts deny = 0.0.0.0/0
+    guest account = nobody
+    map to guest = bad user
+  '';
+  shares = {
+      public = {
+      path = "/mnt/Movies/";
+      browseable = "yes";
+      "read only" = "no";
+      "guest ok" = "yes";
+      "create mask" = "0777";
+      "directory mask" = "0777";
+      "force user" = "username";
+      "force group" = "groupname";
+    };
+  };
+};
+
+ # system.copySystemConfiguration = true;
+ # system.autoUpgrade.enable = true;  
+ # system.autoUpgrade.allowReboot = true; 
+ # system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
+  
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
